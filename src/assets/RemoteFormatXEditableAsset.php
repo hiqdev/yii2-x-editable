4233389738835af64711ea1a13128d6956398e39
Andrii Vasyliev <sol@hiqdev.com>
2017-02-20 12:30:14 +0000

    csfixed

--- src/assets/RemoteFormatXEditableAsset.php
+++ src/assets/RemoteFormatXEditableAsset.php
@@ -1,12 +1,11 @@
 <?php
-
-/*
+/**
  * X-editable extension for Yii2
  *
  * @link      https://github.com/hiqdev/yii2-x-editable
  * @package   yii2-x-editable
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
  */
 
 namespace hiqdev\xeditable\assets;
--- src/messages/ru/xeditable.php
+++ src/messages/ru/xeditable.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * X-editable extension for Yii2
+ *
+ * @link      https://github.com/hiqdev/yii2-x-editable
+ * @package   yii2-x-editable
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
+ */
 
 return [
     'Empty' => 'Пусто',
